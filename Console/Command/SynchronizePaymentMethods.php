ed14fae8ca4edbba01e234430651fcea9e0a1785
andrewrowanwallee <andrew.rowan@wallee.com>
2025-08-06 09:05:03 +0200

    Release 2.1.25

--- Console/Command/SynchronizePaymentMethods.php
+++ Console/Command/SynchronizePaymentMethods.php
@@ -9,12 +9,14 @@
  * @license http://www.apache.org/licenses/LICENSE-2.0  Apache Software License (ASL 2.0)
 
  */
+
 namespace PostFinanceCheckout\Payment\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use PostFinanceCheckout\Payment\Api\PaymentMethodConfigurationManagementInterface;
+use function PHPUnit\Framework\throwException;
 
 /**
  * Command to synchronize the payment methods.
@@ -39,14 +41,30 @@ class SynchronizePaymentMethods extends Command
         $this->paymentMethodConfigurationManagement = $paymentMethodConfigurationManagement;
     }
 
-    protected function configure()
+    /**
+     * @return void
+     */
+    protected function configure(): void
     {
         $this->setName('postfinancecheckout:payment-method:synchronize')->setDescription(
             'Synchronizes the PostFinance Checkout payment methods.');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     * @throws \Exception
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->paymentMethodConfigurationManagement->synchronize($output);
+        try {
+            $this->paymentMethodConfigurationManagement->synchronize($output);
+        } catch (\Exception $e) {
+            $output->writeln('<error>' . $e->getMessage() . '</error>');
+            return 1;
+        }
+
+        return 0;
     }
 }
---
+++ Setup/Patch/Schema/RemoveLock.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace PostFinanceCheckout\Payment\Setup\Patch\Schema;
+
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\SchemaPatchInterface;
+
+class RemoveLock implements SchemaPatchInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $schemaSetup;
+
+    /**
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup
+    ) {
+        $this->schemaSetup = $moduleDataSetup;
+    }
+
+    /**
+     * Drops postfinancecheckout_lock column from sales_order table
+     * @return $this|RemoveLock
+     */
+    public function apply(): self
+    {
+        $setup = $this->schemaSetup;
+        $setup->getConnection()->startSetup();
+
+        $table = $setup->getTable('sales_order');
+
+        $setup->getConnection()->dropColumn($table, 'postfinancecheckout_lock');
+        $setup->getConnection()->endSetup();
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getAliases(): array
+    {
+        return [];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function getDependencies(): array
+    {
+        return [];
+    }
+}
